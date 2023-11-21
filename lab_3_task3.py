7ef3c44a9b648e22d43d5ec43d2093a439f1cc77
jauxusj <144795511+jauxusj@users.noreply.github.com>
2023-11-21 13:37:00 +0200

    thdrsg

--- lab_3_task3.py
+++ lab_3_task3.py
@@ -4,7 +4,7 @@ from lab_3_task1 import yskor_free_pad as g
 x0 = 2
 y0 = 5
 v0X = 10
-t = np.arange(0, 5, 0.001)
+t = np.arange(0, 5, 0.1)
 
 x = x0 + v0X*t
 y = y0 + v0X*t - (g*t**2)/2
@@ -12,4 +12,13 @@ y = y0 + v0X*t - (g*t**2)/2
 print(x)
 print(y)
 
-print(np.zeros((len(x), 3)))
+A = np.zeros((len(x), 3))
+print(A)
+
+for i in range(0, len(x)):
+    A[i, 0] = t[i]
+    A[i, 1] = x[i]
+    A[i, 2] = y[i]
+
+
+print(A)
