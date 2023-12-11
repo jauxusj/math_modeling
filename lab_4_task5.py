e07fb5a34ef14b1a1dfe37b53b326f25a10c2f05
jauxusj <144795511+jauxusj@users.noreply.github.com>
2023-12-12 00:58:05 +0200

    Add files via upload

--- lab_4_task5.py
+++ lab_4_task5.py
@@ -1,9 +1,14 @@
 import numpy as np
 
-def func(a):
-    if a - 'Круг':
-        S = np.pi * 7**2
-    elif a - 'Квадрат':
-        S = b**2
-    print(S)
+def func(figure, r=0, a=0, h=0):
 
+    if figure == 1:
+        S = np.pi * r**2
+    elif figure == 2:
+        S = a**2
+    else:
+        S = a * h/2
+
+    return S
+
+print(func(2, a=3))
