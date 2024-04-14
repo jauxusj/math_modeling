3f153295d27e54119297f59529500dbcf4a88c69
jauxusj <144795511+jauxusj@users.noreply.github.com>
2024-04-14 22:42:28 +0200

    nknkn

--- lec_1_importimage.py
+++ lec_1_importimage.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = plt.imread('n.jpg.jpg')
+img = plt.imread('n.jpg')
 fig, ax = plt.subplots()
 ax.imshow(img, extent=[0, 1000, 0, 200])
 plt.ylim(0, 600)
