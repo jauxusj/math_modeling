b965268dd9ccd1fc440938810feda0a57706ac00
jauxusj <144795511+jauxusj@users.noreply.github.com>
2024-05-06 21:52:52 +0200

    Add files via upload

--- lec_1_head2.py
+++ lec_1_head2.py
@@ -6,7 +6,7 @@ img = plt.imread('H.jpg')
 fig, ax = plt.subplots()
 ax.imshow(img)
 
-t0 = np.linspace(np.pi-np.pi/6.5,np.pi/1.9, 100)
+t0 = np.linspace(np.pi,np.pi/1.9, 100)
 x0 = 390 + 240*np.cos(t0)
 y0 = 800 - 220*np.sin(t0)
 ax.plot(x0, y0, '-', lw=2, color='w')
@@ -34,53 +34,46 @@ ax.plot(x3, y3, '-', lw=2, color='w')
 x = np.append(x, x3)
 y = np.append(y, y3)
 
-t4 = np.linspace(2*np.pi,3*np.pi/2,  100)
+t4 = np.linspace(3*np.pi/2,2*np.pi,  100)
 x4 = 365 + 100*np.cos(t4)
 y4 = 175 - 100*np.sin(t4)
 ax.plot(x4, y4, '-', lw=2, color='w')
 
-# x = np.append(x, x4)
-# y = np.append(y, y4)
+x = np.append(x, x4)
+y = np.append(y, y4)
 
-t5 = np.linspace( np.pi,np.pi/2, 100)
+t5 = np.linspace(np.pi, 0 , 100)
 x5 = 485 + 20*np.cos(t5)
-y5 = 175 - 20*np.sin(t5)
+y5 = 175.5 - 20*np.sin(t5)
 ax.plot(x5, y5, '-', lw=2, color='w')
 
-# x = np.append(x, x5)
-# y = np.append(y, y5)
+x = np.append(x, x5)
+y = np.append(y, y5)
 
-t6 = np.linspace(3*np.pi/2, np.pi,  100)
-x6 = 634 + 120*np.cos(t6)
-y6 = 174 - 20*np.sin(t6)
+t6 = np.linspace(np.pi, 3*np.pi/2, 100)
+x6 = 634.5 + 120*np.cos(t6)
+y6 = 174.5 - 20*np.sin(t6)
 ax.plot(x6, y6, '-', lw=2, color='w')
 
-# x = np.append(x, x6)
-# y = np.append(y, y6)
+x = np.append(x, x6)
+y = np.append(y, y6)
 
-t7 = np.linspace(np.pi/10, np.pi/2, 100)
-x7 = 485 + 30*np.cos(t7)
-y7 = 175 - 20*np.sin(t7)
-ax.plot(x7, y7, '-', lw=2, color='w')
-
-# x = np.append(x, x7)
-# y = np.append(y, y7)
-
-t8 = np.linspace(np.pi/2, -np.pi/2+np.pi/9, 100)
-x8 = 630 + 100*np.cos(t8)
-y8 = 395 - 200*np.sin(t8)
+t8 = np.linspace( np.pi/2, -np.pi/2+np.pi/9,100)
+x8 = 630.5 + 100*np.cos(t8)
+y8 = 395.5 - 200*np.sin(t8)
 ax.plot(x8, y8, '-', lw=2, color='w')
 
-# x = np.append(x, x8)
-# y = np.append(y, y8)
+x = np.append(x, x8)
+y = np.append(y, y8)
 
-t9 = np.linspace(np.pi/2+np.pi/12, np.pi, 100)
+t9 = np.linspace(np.pi/2+np.pi/12, np.pi+np.pi/12, 100)
 x9 = 670 + 35*np.cos(t9)
 y9 = 700 - 120*np.sin(t9)
 ax.plot(x9, y9, '-', lw=2, color='w')
 
-# x = np.append(x, x9)
-# y = np.append(y, y9)
+x = np.append(x, x9)
+y = np.append(y, y9)
+
 
 
 splire_coords, figure_spline_part = interpolate.splprep([x, y], s=0)
@@ -88,5 +81,5 @@ figure_spline_part_manual = np.arange(0, 0.5, 0.01)
 spline_curve = interpolate.splev(figure_spline_part, splire_coords )
 
 plt.plot(spline_curve[0], spline_curve[1], 'c', lw=4)
-
+plt.ylim(700, 0)
 plt.savefig("horseapp.png")
